56802beffcd179135a0b6f1afed32555ce190f30
Nghia <bhtv28@gmail.com>
2021-05-11 23:22:46 +0700

    init project

---
+++ src/test/java/man/openbanking/OpenBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package man.openbanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
